3ba28338a7caf31a6379a6f0a151e73ace900ec2
AttalliAyoub <attalliayoub50@gmail.com>
2021-12-20 18:18:59 +0100

    first code

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { Gpio } from 'onoff';
+
+const trig = new Gpio(21, 'out');
+const echo = new Gpio(20, 'in');
+
+echo.watch((err, value) => {
+    if (err) console.log(err);
+    if (value) console.log(value, Date.now());
+});
+
+const interval = setInterval(() => {
+    if (trig.readSync() === 0) trig.writeSync(1);
+    else trig.writeSync(0);
+}, 0.01);
+
+process.on('SIGINT', () => {
+    trig.unexport();
+    echo.unexport();
+    clearInterval(interval);
+});
