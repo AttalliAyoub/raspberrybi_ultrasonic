7110fc1fdced3e8fb2a72cc91675976fde2cf366
AttalliAyoub <attalliayoub50@gmail.com>
2021-12-20 18:19:31 +0100

    secound code of compile

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var onoff_1 = require("onoff");
+var trig = new onoff_1.Gpio(21, 'out');
+var echo = new onoff_1.Gpio(20, 'in');
+echo.watch(function (err, value) {
+    if (err)
+        console.log(err);
+    if (value)
+        console.log(value, Date.now());
+});
+var interval = setInterval(function () {
+    if (trig.readSync() === 0)
+        trig.writeSync(1);
+    else
+        trig.writeSync(0);
+}, 0.01);
+process.on('SIGINT', function () {
+    trig.unexport();
+    echo.unexport();
+    clearInterval(interval);
+});
